5cf4841aebbfd826b1bb2080fbdd8f04e6802867
Gedson Monteiro <72584422+GedsonMonteiro@users.noreply.github.com>
2021-01-26 13:16:07 -0300

    Add files via upload

---
+++ atividade04/script.js
@@ -0,0 +1,7 @@
+var numero01 = prompt("Digite sua idade");
+var numero02 = parseInt(17)
+
+    if (numero01 > numero02) mensagem = "Você é maior de idade!"
+        else
+    mensagem = "Você é menor de idade!";
+        alert(mensagem);
